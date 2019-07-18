b3d89f92ef9cd9d894e75ca4b33e5e7daf5d2c79
rodrigo-rufino <rodrigo.rufino@local.inatel.br>
2019-07-18 09:24:58 -0300

    implementing dynamic pages with headbars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,31 @@
 const path = require('path');
-const express = require('express');
+const express = require('express'); 
 
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Rodrigo'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        name: 'Rodrigo'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpMessage: 'App to check weather.'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
