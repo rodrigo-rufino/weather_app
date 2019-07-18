2a1ce79ec058c2d0de67c21719e3e49cf566f801
rodrigo-rufino <rodrigo.rufino@local.inatel.br>
2019-07-18 14:30:46 -0300

    creating search bar

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,8 +1,18 @@
-console.log('Client side javascript file is loaded.');
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
 
-fetch('http://localhost:3000/weather?address=Campinas')
-    .then((response) => {
-        response.json().then((data) => {
-            console.log(data);
+weatherForm.addEventListener('submit', (e) => {
+    // prevent browser reload
+    e.preventDefault();
+
+    const location = search.value;
+
+    fetch('http://localhost:3000/weather?address=' + location)
+        .then((response) => {
+            response.json().then((data) => {
+                console.log(data);
+            });
         });
-    });
+
+    console.log(location);
+})
