9217a40ded73f235c0faafbd6be486f47f16bfe7
rodrigo-rufino <rodrigo.rufino@local.inatel.br>
2019-07-18 11:02:10 -0300

    implementing partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,21 @@
 const path = require('path');
-const express = require('express'); 
-
+const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
+// handlebars engine
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// static directory
 app.use(express.static(publicDirectoryPath));
 
+// creating endpoints
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
@@ -17,18 +25,22 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
+        title: 'About',
         name: 'Rodrigo'
     });
 });
 
 app.get('/help', (req, res) => {
     res.render('help', {
+        title: 'Help',
+        name: 'Rodrigo',
         helpMessage: 'App to check weather.'
     });
 });
 
 app.get('/weather', (req, res) => {
     res.send({
+        title: 'Weather',
         forecast: 'Sun.',
         location: 'Santa Rita do Sapucai'
     });
