0c0063eec9682d9d6d90d2e94ca391b6ddb19ed7
rodrigo-rufino <rodrigo.rufino@local.inatel.br>
2019-07-18 14:42:52 -0300

    wiring up user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,16 +1,27 @@
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-one');
+const messageTwo = document.querySelector('#message-two');
 
 weatherForm.addEventListener('submit', (e) => {
     // prevent browser reload
     e.preventDefault();
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
 
     const location = search.value;
 
     fetch('http://localhost:3000/weather?address=' + location)
         .then((response) => {
             response.json().then((data) => {
-                console.log(data);
+                if (data.error){
+                    console.log(data.error);
+                    messageOne.textContent = data.error;
+                } else {
+                    messageOne.textContent = data.location;
+                    messageTwo.textContent = data.forecast;
+                }
             });
         });
 
